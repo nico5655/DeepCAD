ab42b194c3d9cfec5e6a4a0af36aa386265e8ba6
nico5655 <nicoukia.gamme@gmail.com>
2025-07-16 17:39:58 +0200

    orion

--- cadlib/visualize.py
+++ cadlib/visualize.py
@@ -147,7 +147,9 @@ def CADSolid2views(shape,n_views,name=None):
         name = random.randint(100000, 999999)
     write_stl_file(shape, "tmp_out_{}.stl".format(name))
     out_mesh = trimesh.load("tmp_out_{}.stl".format(name))
-    print(os.system(f"cd ../ && ./blender-3.6.0-linux-x64/blender --background --python render.py -- {out_mesh} {name}"))
+    command=f"cd ../ && ./blender-3.6.0-linux-x64/blender --background --python render.py -- {out_mesh} {name}"
+    print(command)
+    print(os.system(command))
     os.system("rm tmp_out_{}.stl".format(name))
     imgs=[]
     for i in range(n_views):
